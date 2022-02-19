79288332bb91835c2d6c526bc4a72d41c8e72d3b
delivey <deliveydev@gmail.com>
2022-02-19 19:19:34 +0200

    Kinda broken enter function

--- game.js
+++ game.js
@@ -2,22 +2,36 @@ function isLetter(s) {
   return s.match("^[a-zA-Z\(\)]+$") && s.length === 1;    
 }
 
+var lastRowAccepted = -1;
+
 function main() {
     const input = document.getElementById("input")
     console.log("input")
     input.onkeydown = function(e){
-        const key = e.key.toUpperCase();
+        let key = e.key
         if (isLetter(key)) {
+            key = key.toUpperCase();
             const blocks = document.getElementsByClassName("block");
-            for (let block of blocks) {
+            for (let i=0; i<blocks.length; ++i) {
+                const block = blocks[i]
+                console.log(block)
                 if (!block.lastChild) {
-                    let letterElement = document.createElement("p");
-                    let letter = document.createTextNode(key);
-                    letterElement.appendChild(letter)
-                    block.appendChild(letterElement);
-                    break;
+                    console.log(lastRowAccepted)
+                    if (i < 5 || lastRowAccepted > 0) {
+                        let letterElement = document.createElement("p");
+                        let letter = document.createTextNode(key);
+                        letterElement.appendChild(letter)
+                        block.appendChild(letterElement);
+                        if (i > 5) lastRowAccepted++;
+                        if (lastRowAccepted === 5) lastRowAccepted = -1;
+                        break;
+                    }
                 }
             }
+        }
+        if (key === "Enter") {
+            console.log("Entered")
+            lastRowAccepted = 1;
         }
     };
     
