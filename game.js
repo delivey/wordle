09f35e10da215451b6b3057a353c7e75badb946b
delivey <deliveydev@gmail.com>
2022-02-19 18:51:25 +0200

    Fixed non-letters being put into the board

--- game.js
+++ game.js
@@ -1,18 +1,22 @@
+function isLetter(s) {
+  return s.match("^[a-zA-Z\(\)]+$") && s.length === 1;    
+}
+
 function main() {
     const input = document.getElementById("input")
     console.log("input")
     input.onkeydown = function(e){
-        const key = e.key
-        const blocks = document.getElementsByClassName("block");
-        for (let block of blocks) {
-            console.log(block)
-            if (!block.lastChild) {
-                let letterElement = document.createElement("p");
-                let letter = document.createTextNode(key);
-                letterElement.appendChild(letter)
-                console.log(letterElement)
-                block.appendChild(letterElement);
-                break;
+        const key = e.key.toUpperCase();
+        if (isLetter(key)) {
+            const blocks = document.getElementsByClassName("block");
+            for (let block of blocks) {
+                if (!block.lastChild) {
+                    let letterElement = document.createElement("p");
+                    let letter = document.createTextNode(key);
+                    letterElement.appendChild(letter)
+                    block.appendChild(letterElement);
+                    break;
+                }
             }
         }
     };
